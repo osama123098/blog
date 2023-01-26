405437051e11fd3617f655d64fa732358ed21529
osama123098 <osamashaikhansari@gmail.com>
2023-01-26 05:32:40 -0500

    Blog_app complete

--- blogs_app/views.py
+++ blogs_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView, DeleteView
+from .models import Post
 # Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DeleteView):
+    model = Post
+    template_name = "post_detail.html"
